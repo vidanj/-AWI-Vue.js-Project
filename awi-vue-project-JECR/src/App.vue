d30246a474d4c698b44079b9775c08c49d3b8cb4
Efrén Campuzano <efrecamp@hotmail.com>
2025-02-03 05:02:01 -0700

    Lista de tareas

--- awi-vue-project-JECR/src/App.vue
+++ awi-vue-project-JECR/src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 
 import Contador from './components/Contador.vue';
+import ListaDeTareas from './components/ListaDeTareas.vue';
 
 </script>
 
 <template>
 <Contador></Contador>
+<ListaDeTareas></ListaDeTareas>
 </template>
 
 
---
+++ awi-vue-project-JECR/src/components/ListaDeTareas.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue';
+
+const tareas = ref([]);
+const nuevaTarea = ref('');
+const addTarea = () => {
+    tareas.value.push(nuevaTarea.value);
+    nuevaTarea.value = '';
+}
+</script>
+
+<template>
+    <div>
+        <h2>Lista de tareas</h2>
+        <input type="text" v-model="nuevaTarea" @keyup.enter="addTarea">
+        <ul>
+            <li v-for="(tarea,index) in tareas" :key ='index'>
+            {{ tarea }}
+            </li>
+
+        </ul>
+    </div>
+</template>
+
+
+<style>
+
+</style>

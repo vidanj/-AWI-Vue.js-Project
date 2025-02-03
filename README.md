d2a61a6e97eb7eb08a5b2be262ac4cd7f9a8a6ab
Efrén Campuzano <efrecamp@hotmail.com>
2025-02-03 04:46:01 -0700

    Agregado info a Readme Github y Realizado ejercicio Contador

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # -AWI-Vue.js-Project
+
+## Autor
+- Nombre: Jesús Efrén Campuzano rangel
+- Email: a20311009@uthermosillo.edu.mx
+- GitHub: [@vidanj](https://github.com/vidanj)
---
+++ awi-vue-project-JECR/src/components/Contador.vue
@@ -0,0 +1,28 @@
+<template>
+    <h1>Hola Mundo</h1>
+   <p>{{ contador }}</p>
+   <button @click="incrementar">incrementar</button>
+   <button @click="decremetar">decremetar</button>
+  </template>
+  
+  <script setup>
+  import {ref} from 'vue';
+  const contador =  ref(0);
+  
+  const incrementar = () => {
+    contador.value++;
+  }
+  const decremetar = () => {
+    contador.value--;
+  }
+  
+  </script>
+  
+  <style scoped>
+  
+  p {
+    color: red;
+  }
+  
+  </style>
+  
